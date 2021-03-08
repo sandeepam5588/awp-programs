b1699e96b7c86590b2967eefbb9d44866516257e
Sandeepa M <sandeepmreddy5588@gmail.com>
2021-03-08 08:51:45 +0530

    updated program6

--- program6.pl
+++ program6.pl
@@ -1,8 +1,9 @@
-#!/usr/bin/perl.exe
- use CGI':standard';
- #refresh the display every second
- print "Refresh:1\n";
- print "Content-type:text/html\n\n";
- ($s,$m,$h)=localtime(time);
- print "The current server time is $h:$m:$s";
- print "In words,the time is- $h hours, $m minutes and $s seconds";
+#!"D:\xampp\perl\bin\perl.exe"
+
+print "refresh:1 \n";
+print "content-type:text/html \n\n";
+
+use CGI':standard';
+($s,$m,$h)=localtime(time);
+print "$h:$m:$s";
+print "<br/><br/>$h hours $m minutes $s seconds";
